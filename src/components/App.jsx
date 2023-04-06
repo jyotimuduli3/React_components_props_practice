35c12d13fe86c80576b4c5c43831eecfc0d4d081
JYOTIRANJAN MUDULI <92794275+jyotimuduli3@users.noreply.github.com>
2023-04-07 03:49:55 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://media.licdn.com/dms/image/C4D03AQG58XNSJfLqHw/profile-displayphoto-shrink_800_800/0/1641753239210?e=2147483647&v=beta&t=3aeHlYcEonfVtwJQhxc8IMg7klmP19nJpcP9Qk-Ingw" />
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Detail.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+function Detail(props) {
+  return <p className="info">{props.detailedinfo}</p>;
+}
+export default Detail;
